808a276365b4b932ba80153ec68746afaccebae6
Jesse <jesse.lange92@gmail.com>
2018-11-18 13:06:28 +0100

    2nd Commit: Intializition Done. Next: Create Repository Methods

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO category (categoryName) VALUES ('American');
+INSERT INTO category (categoryName) VALUES ('Italian');
+INSERT INTO category (categoryName) VALUES ('Mexican');
+INSERT INTO category (categoryName) VALUES ('Fast Food');
+
+INSERT INTO unit_of_measure (describtion) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (describtion) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (describtion) VALUES ('Cup');
+INSERT INTO unit_of_measure (describtion) VALUES ('Pinch');
+INSERT INTO unit_of_measure (describtion) VALUES ('Ounce');
